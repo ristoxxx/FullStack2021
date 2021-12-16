8f26a74203931eb8a9003df209189c4111ecf94c
ristoxxx <lahteri@gmail.com>
2021-12-16 16:48:34 +0200

    harjoitus 1.0

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const Heippa = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Heippa {props.nimi}</p>
     </div>
-  );
+  )
 }
 
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Heippa nimi="s3Xy" />
+      <Heippa nimi="Kitty" />
+    </div>
+  )
+}
 export default App;
