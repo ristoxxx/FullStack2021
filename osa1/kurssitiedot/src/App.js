62e31cfa8269f194ff456fb985c1e68f2efad93e
ristoxxx <lahteri@gmail.com>
2021-12-16 18:24:01 +0200

    step2

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -14,7 +14,9 @@ const Content = (props) => {
   return (
     <div>
       <p>
-         {props.part1} <br/><br/> {props.part2} <br/><br/> {props.part3}
+        <Part part={props.part1} />
+        <Part part={props.part2} />
+        <Part part={props.part3} />
       </p>
     </div>
   )
@@ -30,6 +32,16 @@ const Total = (props) => {
   )
 }
 
+const Part = (props) => {
+    return (
+      <div>
+        <p>
+          {props.part}
+        </p>
+      </div>
+    )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -42,7 +54,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1+" "+ exercises1} part2={part2+ " "+ exercises2} part3={part3+" "+exercises3} />
+      <Content  part1={part1+" "+ exercises1} part2={part2+ " "+ exercises2} part3={part3+" "+exercises3}/>
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
