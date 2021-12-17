7fe001339ea9a7fdae02d0490687def707c5ef0e
ristoxxx <lahteri@gmail.com>
2021-12-17 19:01:32 +0200

    step1

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,24 +1,53 @@
-import React from react;
+import React, {useState} from "react";
 
-function App() {
+const App = () => {
+  const [huono, setHuono] = useState(0)
+  const [hyva, setHyva] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleHuonoClick = () => {
+    setAll(allClicks.concat('L'))
+    setHuono(huono + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setAll(allClicks.concat('N'))
+    setNeutral(neutral + 1)
+  }
+
+  const handleHyvaClick = () => {
+    setAll(allClicks.concat('R'))
+    setHyva(hyva + 1)
+  }
+  
+  const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1>Anna palautetta</h1>
+        
+        
+        <Button handleClick={handleHyvaClick} text='hyva' />
+        <Button handleClick={handleNeutralClick} text='neutraali' />
+        <Button handleClick={handleHuonoClick} text='huono' />
+        
+    
+
+        <h1>Tilasto</h1>
+        <p>Hyvä {hyva}</p>
+        <p>Neutraali {neutral}</p>
+        <p>Huono {huono}</p>
+        
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
