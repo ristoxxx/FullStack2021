805945e15b2b30d272056097a413a96887c4627d
ristoxxx <lahteri@gmail.com>
2021-12-17 12:58:33 +0200

    sekuntilaskuri

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,28 +1,10 @@
 import React from "react";
 
-const Heippa = ({name, age}) => {
-  
-  const bornYear = () =>  new Date().getFullYear() - age
-
-
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
-      <p>Heippa {name}, oot {age} vuotias</p>
-      <p>Joten oot varmaan syntynyt {bornYear()}</p>
-    </div>
+    <div>{counter}</div>
   )
 }
 
-const App = () => {
-  const nimi = 'Hello'
-  const ika = 20
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Heippa name={nimi} age={ika}  />
-      <Heippa name="Kitty" age={6+5}/>
-    </div>
-  )
-}
 export default App;
