4ce35e8a9d349b82aa02107d2ce29031c22ee772
ristoxxx <lahteri@gmail.com>
2021-12-17 12:15:22 +0200

    apufunktiot

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,19 +1,28 @@
 import React from "react";
 
 const Heippa = (props) => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
+
   return (
     <div>
-      <p>Heippa {props.nimi}</p>
+      <p>Heippa {props.name}, oot {props.age} vuotias</p>
+      <p>Joten oot varmaan syntynyt {bornYear()}</p>
     </div>
   )
 }
 
 const App = () => {
+  const nimi = 'Hello'
+  const ika = 10
+
   return (
     <div>
       <h1>Greetings</h1>
-      <Heippa nimi="s3Xy" />
-      <Heippa nimi="Kitty" />
+      <Heippa name={nimi} age={ika}  />
+      <Heippa name="Kitty" age={6+5}/>
     </div>
   )
 }
