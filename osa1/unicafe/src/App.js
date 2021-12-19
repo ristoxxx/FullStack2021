1299a681a7c36b2b7dea20b101427677a87b5429
ristoxxx <lahteri@gmail.com>
2021-12-19 11:48:51 +0200

    step2v1

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -4,21 +4,28 @@ const App = () => {
   const [huono, setHuono] = useState(0)
   const [hyva, setHyva] = useState(0)
   const [neutral, setNeutral] = useState(0)
-  const [allClicks, setAll] = useState([])
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [points, setPoints] = useState(0)
 
   const handleHuonoClick = () => {
-    setAll(allClicks.concat('L'))
+    setAll(all + 1)
     setHuono(huono + 1)
+    setPoints(points - 1)
+    calculateAverage()
   }
 
   const handleNeutralClick = () => {
-    setAll(allClicks.concat('N'))
+    setAll(all +1)
     setNeutral(neutral + 1)
+    calculateAverage()
   }
 
   const handleHyvaClick = () => {
-    setAll(allClicks.concat('R'))
+    setAll(all + 1)
     setHyva(hyva + 1)
+    setPoints(points + 1)
+    calculateAverage()
   }
   
   const Button = ({ handleClick, text }) => (
@@ -27,23 +34,28 @@ const App = () => {
     </button>
   )
 
+  const calculateAverage = () => {
+    setAverage(points/all)
+  }
+
   
   return (
     <div>
       <div>
         <h1>Anna palautetta</h1>
-        
-        
+  
         <Button handleClick={handleHyvaClick} text='hyva' />
         <Button handleClick={handleNeutralClick} text='neutraali' />
         <Button handleClick={handleHuonoClick} text='huono' />
         
-    
-
         <h1>Tilasto</h1>
+
         <p>Hyvä {hyva}</p>
         <p>Neutraali {neutral}</p>
         <p>Huono {huono}</p>
+        <p>Kaikki {all}</p>
+        <p>Keskiarvo {average}</p>
+        <p>pisteet {points}</p>
         
       </div>
     </div>
